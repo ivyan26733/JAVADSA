447656e39c110d63aec3f631796bcb7245d82516
Ivyan <ds26733@gmail.com>
2023-11-03 18:16:18 +0530

    Ivyan(Coder 2.0)

---
+++ src/printJumpmazepath.java
@@ -0,0 +1,23 @@
+public class printJumpmazepath {
+    public static void main(String[] args) {
+        printpath(1,1,4,4,"");
+    }
+
+    public static void printpath(int sr , int sc , int dr , int dc,String psf){
+        if (dr==sr && dc==sc){
+            System.out.println(psf);
+            return;
+        }
+
+        for (int i=1;i<=dc-sc;i++){
+            printpath(sr,sc+i,dr,dc,psf+'h'+i);
+        }
+        for (int j=1;j<=dr-sr;j++){
+            printpath(sr+j,sc,dr,dc,psf+'v'+j);
+        }
+        for (int i=1;i<=dr-sr&&i<=dc-sc;i++){
+            printpath(sr+i,sc+i,dr,dc,psf+'d'+i);
+        }
+
+    }
+}
---
+++ src/printmazepath.java
@@ -0,0 +1,19 @@
+public class printmazepath {
+    public static void main(String[] args) {
+        mazepath(1,1,4,4,"");
+    }
+
+    public static void mazepath(int sr , int sc , int dr , int dc,String psf){
+        if (sr>dr || sc>dc){
+            return;
+        }
+        if (dr==sr && dc==sc){
+            System.out.println(psf);
+            return;
+        }
+
+        mazepath(sr+1,sc,dr,dc,psf+'v');
+
+        mazepath(sr,sc+1,dr,dc,psf+'h');
+    }
+}
