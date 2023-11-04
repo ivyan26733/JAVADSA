0861593d31a645ebbeb556d2b2581c4dd20b7551
Ivyan <ds26733@gmail.com>
2023-11-04 23:10:14 +0530

    Ivyan(Coder 2.0)

--- src/PrintSubset.java
+++ src/PrintSubset.java
@@ -17,6 +17,7 @@ public class PrintSubset {
     // sos is sum of subset
     // tar is target
     public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+        //Base case should be that it should check that sum of subset should be equls to target
         if (idx==arr.length) {
             if (sos == tar) {
                 System.out.println(set + ",");
@@ -24,7 +25,9 @@ public class PrintSubset {
             return;
         }
 
+        //In this first recursive should be like index increase by one , in subset element will be stored and in sos element will be added
         printTargetSumSubsets(arr , idx+1 , set+arr[idx] + " , ",sos + arr[idx], tar);
+        //In second call , only index will be increase
         printTargetSumSubsets(arr , idx+1 , set , sos , tar);
     }
 }
